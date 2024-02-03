08f0ce10adbb68d92948b3d95d0a7d637103b551
Lukasz Jachym <lukasz.jachym@gmail.com>
2024-02-03 09:26:16 +0100

    adds: initial implementation of Statsd protocol using UDP connection

---
+++ aiostatsd/metrics.py
@@ -0,0 +1,69 @@
+from dataclasses import dataclass, field
+from datetime import timedelta
+from typing import Optional, Union
+
+
+class Metric:
+    def __init__(self, name: str, value, unit: str, rate: float = 1.0):
+        self.name = name
+        self.value = value
+        self.rate = rate
+        self.unit = unit
+
+    def get_value(self) -> str:
+        return f"{self.value}"
+
+    def serialize(self) -> Optional[str]:
+        value = self.get_value()
+        serialized = f"{self.name}:{value}|{self.unit}"
+        if self.rate < 1:
+            serialized = f"{serialized}|@{self.rate}"
+        return serialized
+
+
+@dataclass
+class StatsdMetric(Metric):
+    name: str
+    value: int
+    unit: str
+    rate: float = field(default=1.0)
+
+    def __post_init__(self):
+        super().__init__(self.name, self.value, self.unit, self.rate)
+
+
+@dataclass
+class CounterMetric(StatsdMetric):
+    unit: str = field(default="c", init=False)
+    value: int = field(default=1)
+
+
+@dataclass
+class GaugeMetric(StatsdMetric):
+    unit: str = field(default="g", init=False)
+    delta: bool = field(default=False)
+
+    def get_value(self) -> str:
+        value = self.value
+        if self.delta:
+            sign = "+" if self.value > 0 else ""
+            value = f"{sign}{value}"
+        return value
+
+
+@dataclass
+class SetMetric(StatsdMetric):
+    unit: str = field(default="s", init=False)
+
+
+@dataclass
+class TimingMetric(StatsdMetric):
+    value: Union[float, timedelta]
+    unit: str = field(default="ms", init=False)
+
+    def get_value(self) -> str:
+        delta = self.value
+        if isinstance(delta, timedelta):
+            # Convert timedelta to number of milliseconds.
+            delta = delta.total_seconds() * 1000.0
+        return f"{delta:.6f}"
---
+++ aiostatsd/udp.py
@@ -0,0 +1,61 @@
+import asyncio
+from typing import Optional
+
+
+class StatsdProtocol(asyncio.DatagramProtocol):
+    def __init__(self, on_con_lost: Optional[asyncio.Future] = None):
+        self.on_con_lost = on_con_lost
+        self.transport = None
+
+    def connection_made(self, transport):
+        self.transport = transport
+        print("Connection made")
+
+    def error_received(self, exc):
+        print("Error received:", exc)
+
+    def connection_lost(self, exc):
+        if self.on_con_lost is not None:
+            self.on_con_lost.set_exception(exc)
+        print("Closing transport", exc)
+
+
+class Client:
+    async def connect(self):
+        raise NotImplementedError
+
+    async def send(self, data):
+        raise NotImplementedError
+
+    def close(self):
+        raise NotImplementedError
+
+
+class UDPClient:
+    def __init__(self, host, port, maxudpsize=512):
+        self._host = host
+        self._port = port
+        self._maxudpsize = maxudpsize
+        self._loop = asyncio.get_event_loop()
+        self._transport = None
+        self._on_con_lost = None
+
+    def close(self):
+        if self._transport:
+            self._transport.close()
+            self._transport = None
+
+    async def connect(self):
+        self._on_con_lost = self._loop.create_future()
+        self._transport, _ = await self._loop.create_datagram_endpoint(
+            lambda: StatsdProtocol(self._on_con_lost), remote_addr=(self._host, self._port)
+        )
+        self._on_con_lost.add_done_callback(lambda f: self._close())
+
+    async def send(self, data):
+        encoded = data.encode("ascii")
+        if len(encoded) > self._maxudpsize:
+            raise ValueError("Data exceeds max UDP size")
+        if self._transport is None:
+            await self.connect()
+        self._transport.sendto(encoded)
